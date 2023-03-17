23d1d7edba5f8207dbebf03a421eaa64f3f09880
cheetah112 <nonpvp123@gmail.com>
2023-03-17 14:55:00 +0800

    ok

--- shopping-cart-frontend/src/App.jsx
+++ shopping-cart-frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ShoppingCart from './assets/shoppingCart/ShoppingCart'
+import ShoppingCart from './shoppingCart/ShoppingCart'
 
 function App() {
   const [count, setCount] = useState(0)
